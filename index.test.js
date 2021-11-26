d78e65fe0fb27d74eceb69f48c2f785a9894bdba
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-26 12:08:52 +0000

    Add test for deleting item from list add mock html elements to demonstrate test failure

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
-//Test for submitting a new task to be added
-// to the to-do list
+// test for submitting a new task to be added to the to-do list
 
 test("Once a user adds an item, the result is shown", () => {
     const form = document.querySelector("form");
@@ -11,7 +10,25 @@ test("Once a user adds an item, the result is shown", () => {
     equal(addedItem.textContent, "I'm new!");
     //input.value = "";
     form.reset()
+})
 
+// test deleting an item from the to-do list
+test("Delete button removes item and updates list", () => {
+    // grab list
+    const list = document.querySelector('ul');
+    console.dir(list);
+    // find current num of ul child elements
+    const numOfChildren = list.childElementCount;
+    console.log(numOfChildren)
+    // grab the delete button
+    const deleteBtn = document.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+    // click it
+    deleteBtn.click();
+    // check that the number of childern equals the current num - 1
+    equal(list.childElementCount, numOfChildren - 1);
 })
 
 
