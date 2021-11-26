0392f9b47c349ea85cd421db747080802f29d9b3
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-26 12:37:34 +0000

    Add test for toggling a filter button to hide completed tasks code to be refactored

--- index.test.js
+++ index.test.js
@@ -12,16 +12,16 @@ test("Once a user adds an item, the result is shown", () => {
     form.reset()
 })
 
-// test deleting an item from the to-do list
-test("Delete button removes item and updates list", () => {
+// test for deleting an item from the to-do list
+test("Deleting an item removes it from the list", () => {
     // grab list
     const list = document.querySelector('ul');
-    console.dir(list);
     // find current num of ul child elements
     const numOfChildren = list.childElementCount;
-    console.log(numOfChildren)
     // grab the delete button
     const deleteBtn = document.querySelector('.delete-btn');
+    // prevent page refresh on btn click
+    // I will move this into index.js when we start coding the app
     deleteBtn.addEventListener('click', (e) => {
         e.preventDefault();
     })
@@ -29,7 +29,25 @@ test("Delete button removes item and updates list", () => {
     deleteBtn.click();
     // check that the number of childern equals the current num - 1
     equal(list.childElementCount, numOfChildren - 1);
-})
+});
 
-
-
+// test for toggling a button to hide completed items
+test("Toggling the filter hides completed tasks from the list", () => {
+    // grab the filter button
+    const filterBtn = document.querySelector('#filter-btn');
+    // prevent page refresh on btn click
+    filterBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+    // click it
+    filterBtn.click();
+    // if the item has a classlist of 'completed-task' it should be hidden
+    // get all list items as array
+    const allItems = Array.from(document.querySelectorAll('li'));
+    // check they contain a classlist of 'completed-task' and a hidden attribute of true
+    if (allItems.every(item => item.classList.contains('completed-task') && item.hidden === true)) {
+        console.info("Pass: completed tasks are hidden")
+    } else {
+        console.error("Fail: completed tasks are not hidden")
+    };
+  });
