8e10a2d56975480e542089f044772f7abe9bd650
MillyP <millypalmer1@hotmail.co.uk>
2021-11-28 19:16:09 +0000

    Tests for adding an item and completing an item

--- index.test.js
+++ index.test.js
@@ -1,18 +1,25 @@
-//Test for submitting a new task to be added
-// to the to-do list
+// Test for adding a new task to the to-do list
 
 test("Once a user adds an item, the result is shown", () => {
     const form = document.querySelector("form");
-    const input = document.querySelector("#task"); 
+    const input = document.querySelector("input[id='task']"); 
     input.value = "I'm new!";
-    const addedItem = document.querySelector("#result")
-    const btn = document.querySelector("#btn");
+    const addedItem = document.querySelector("output[id='result']")
+    const btn = document.querySelector("button[id='btn']");
     btn.click();
     equal(addedItem.textContent, "I'm new!");
-    //input.value = "";
     form.reset()
 
 })
 
+// Test for ticking completed items off the to-do list
 
+test("Once a user has completed a task, a line goes through it", () => {
+    const taskSection = document.querySelector("section[id='task-section']");
+    const input = document.querySelector("input[id='task']"); 
+    input.value = "Item done!";
+    const btnDone = document.querySelector("button[id='btn-done']");
+    btnDone.click();
+    equal(taskSection.textContent, "Item done!");
+})
 
