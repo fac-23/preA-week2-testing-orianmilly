6064ff1839b39fddf11cf61806701a28f7e1e264
MillyP <millypalmer1@hotmail.co.uk>
2021-11-29 23:04:58 +0000

    Refactored add test and complete test

--- index.test.js
+++ index.test.js
@@ -1,25 +1,41 @@
 // Test for adding a new task to the to-do list
 
-test("Once a user adds an item, the result is shown", () => {
-    const form = document.querySelector("form");
-    const input = document.querySelector("input[id='task']"); 
-    input.value = "I'm new!";
-    const addedItem = document.querySelector("output[id='result']")
-    const btn = document.querySelector("button[id='btn']");
-    btn.click();
-    equal(addedItem.textContent, "I'm new!");
-    form.reset()
-
-})
+test("Submitting a new task adds it to the list", () => {
+    // Grab the input tag from html with an id of task
+    const input = document.querySelector("input[id='task']");
+    // Add a value to the input to test it's working
+    input.value = "New task";
+    // Write an if statement to check if the box contains text
+    if(input.value !== "") {
+        console.info(`Pass: Task contains text`)
+    } else {
+        console.error(`Fail: Task doesn't contain text`)
+    }
+    // grab add button element for submitting a task
+    const addBtn = document.querySelector("button[id='add-btn']")
+    // Event click on add button for user submit their task
+    addBtn.addEventListener('click', (event) => {
+    // Stop page from reloading once task is added
+        event.preventDefault();
+        })
+    })
 
 // Test for ticking completed items off the to-do list
 
-test("Once a user has completed a task, a line goes through it", () => {
-    const taskSection = document.querySelector("section[id='task-section']");
-    const input = document.querySelector("input[id='task']"); 
-    input.value = "Item done!";
-    const btnDone = document.querySelector("button[id='btn-done']");
-    btnDone.click();
-    equal(taskSection.textContent, "Item done!");
+test("Checking an entry marks it as complete", () => {
+    // Grab the input tag with a class of task-added
+    const taskAdded = document.querySelector("input[class='task-added']");
+    // Grab the button tag with id of complete-btn
+    const completeBtn = document.querySelector("button[id='complete-btn']");
+    // If the user clicks the complete button, complete class is added
+    completeBtn.addEventListener('click', (event) => {
+        if (event === true) {
+            console.info(`Pass: Task is complete`)
+    // Or else the complete class is hidden
+        } else {
+            console.error(`Fail: Task is not complete`)
+        }
+    })
+
 })
 
