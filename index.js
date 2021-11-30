fe0a85d14491c8bd691bf038345b4d4ffd000b88
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-30 11:01:00 +0000

    Refactor deleting function and deleting test hide deleted task card instead of removing node from the DOM. Refactor test to check that a deletedTasks variable is incremented upon clicking the delete button. Fixes #8

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const deleteBtn = document.querySelector('.delete-btn');
+let deletedTasks = 0;
 
 deleteBtn.addEventListener('click', (e) => {
     deleteTask(e);
@@ -6,5 +7,9 @@ deleteBtn.addEventListener('click', (e) => {
 
 // delete task from list
 function deleteTask(e) {
-    e.target.parentElement.parentElement.remove();
+    // increment deletedTasks variable to check in tests
+    deletedTasks++;
+    // hide the task card that is being deleted
+    e.target.parentElement.parentElement.setAttribute("hidden", true);
 }
+
