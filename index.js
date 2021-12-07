dcbdaa57a4529df6ddf8cb1709adbabe3675f282
Orian <77785778+OrianP@users.noreply.github.com>
2021-12-07 21:10:41 +0000

    Edit btn and tests declare isEditing flag to control the editing functionality instead of the button text content which is now set to emojis. assign taskNew a value in the tests and clear it at end of tests

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ let editBtn;
 let deleteBtn;
 let deletedTasks = 0;
 let isHidden = false;
+let isEditing = false;
 
 // add event listener to button
 addBtn.addEventListener('click', (e) => {
     //e.preventDefault();
     // get value of task from input
-
-    const taskValue = document.querySelector('input#task').value;
-    if(taskValue !== "") {
+    let userInput = document.querySelector('input#task');
+    // if user has entered a value, create a new task
+    if(userInput.value !== "") {
         e.preventDefault();
-    
     // create a div with a class of task-card
     const taskCard = document.createElement('div');
     taskCard.classList.add('task-card');
@@ -25,7 +25,7 @@ addBtn.addEventListener('click', (e) => {
     // create an input with class task-new
     const taskNew = document.createElement('input');
     taskNew.classList.add('task-new');
-    taskNew.value = taskValue;
+    taskNew.value = userInput.value;
     taskNew.setAttribute('readonly', 'readonly');
     
     // set checkboxes to appear once task has been added 
@@ -63,21 +63,34 @@ addBtn.addEventListener('click', (e) => {
     // append controls to task card
     taskCard.append(taskNew, controls);
     taskList.append(taskCard);
+    // clear the user input field
+    userInput.value = "";
+
+    
 
     // event listener for edit button
     editBtn.addEventListener('click', () => {
-        console.log(editBtn)
-        if (editBtn.textContent === 'Save') {
+        // if we are in editing mode, clicking the button will save the changes
+        if (isEditing) {
             // make new input value readonly
             taskNew.setAttribute('readonly', 'readonly');
-            editBtn.textContent = 'Edit';
+            // reset button to edit emoji
+            editBtn.innerHTML = editBtn.innerHTML = String.fromCodePoint(0x1F58B)
+            // set isEditing to False 
+            isEditing = false;
+            console.log(isEditing);
+            console.dir(editBtn);
         } else {
-            // change button text content
+            // this code block will run first because isEditing is initially set to false
+            // change button to save emoji
             editBtn.innerHTML = String.fromCodePoint(0x1F4CC);
             // enable input editing
             taskNew.removeAttribute('readonly');
             // focus on input in edit mode
             taskNew.focus();
+            // set isEditing to true
+            isEditing = true;
+            console.log(isEditing);
         }
     })
 
@@ -86,9 +99,10 @@ addBtn.addEventListener('click', (e) => {
     // hide task card
     deleteBtn.addEventListener('click', () => {
         deletedTasks++;
+        // taskCard.remove();
         taskCard.style.display = "none";
         })
-    }
+    }  
 })
 
 filterBtn.addEventListener('click', () => {
--- index.test.js
+++ index.test.js
@@ -1,19 +1,25 @@
-
+// grab the input to assign and remove value in the tests
+let taskNew = document.querySelector('input#task');
 // Test for adding a new task to the to-do list
 
 test("Submitting a new task adds it to the list", () => {
     const before = taskList.childElementCount;
+    // give the input a value otherwise new task will not be created
+    taskNew.value = "New task";
     addBtn.click();
     const after = taskList.childElementCount;
     equal(after, before + 1)
     const taskCard = document.querySelector(".task-card")
-    taskCard.remove(); 
+    taskCard.remove();
+    // reset value to empty string
+    taskNew.value = ""; 
     })
 
 
 // Test for ticking completed items off the to-do list
 
 test("Checking an entry marks it as complete", () => {
+    taskNew.value = "New task";
     // Click add button
         addBtn.click(); 
     // grab checkbox from local scope
@@ -27,12 +33,15 @@ test("Checking an entry marks it as complete", () => {
     // equal function checks that the completed task is true
         equal(taskCardDone, true)
         taskCard.remove();
+        // reset value to empty string
+        taskNew.value = ""; 
     });
 
 // test for deleting an item from the to-do list
 test("Deleting an item removes it from the list", () => {
     // grab current number of deleted tasks (global variable in index.js)
     const currDeletedTasks = deletedTasks;
+    taskNew.value = "New task";
     // click add button to create task card with delete button inside
     addBtn.click();
     // click delete button
@@ -44,10 +53,12 @@ test("Deleting an item removes it from the list", () => {
     // remove test task card that was created in the test
     const taskCard = document.querySelector('.task-card');
     taskCard.remove();
+    taskNew.value = "";
 });
 
 // test for edit button
 test("Clicking the edit button enables editing", () => {
+    taskNew.value = "New task";
     // add new task
     addBtn.click();
     // click edit button
@@ -58,10 +69,12 @@ test("Clicking the edit button enables editing", () => {
     // remove test task card
     const taskCard = document.querySelector('.task-card');
     taskCard.remove();
+    taskNew.value = "";
 })
 
 // test for toggling a button to hide completed items
 test("Toggling the filter hides completed tasks from the list", () => {
+    taskNew.value = "New task";
     // add new task
     addBtn.click();
     // grab task card
@@ -74,5 +87,6 @@ test("Toggling the filter hides completed tasks from the list", () => {
     // check task card contain a classlist of 'completed-task' and a hidden attribute of true
     equal((taskCard.classList.contains('completed-task') && taskCard.style.display === "none"), true,'completed tasks are hidden')
     taskCard.remove();
+    taskNew.value = "";
     filterBtn.textContent = "Hide completed tasks";
 });
